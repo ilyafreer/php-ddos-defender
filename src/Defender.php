3de7425d46e7ed6a88c3ba1fd84c9f4ae416afe5
Ilya <freer@mail.ru>
2023-05-02 21:50:23 +0400

    update composer

--- src/Defender.php
+++ src/Defender.php
@@ -1,11 +1,8 @@
 <?php
 declare(strict_types=1);
 
-namespace DdosDefender;
+namespace Ilyafreer\DdosDefender;
 
-/**
- * 
- */
 final class Defender
 {
     private const PREFIX_FILE_NAME = 'ddos_filter_';
